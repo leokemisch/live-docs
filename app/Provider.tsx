e5a88de366a24d2c2048bb10fb6af71d2f78c43a
Leonardo Kemisch <leonardokemisch@gmail.com>
2024-08-26 15:50:30 -0300

    feat: user mention

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -3,9 +3,12 @@
 import { ReactNode } from 'react';
 import { ClientSideSuspense, LiveblocksProvider } from '@liveblocks/react';
 import Loader from '@/components/Loader';
-import { getClerkUsers } from '@/lib/actions/user.actions';
+import { getClerkUsers, getDocumentUsers } from '@/lib/actions/user.actions';
+import { useUser } from '@clerk/nextjs';
 
 const Provider = ({ children }: { children: ReactNode }) => {
+  const { user: clerkUser } = useUser();
+
   return (
     <LiveblocksProvider
       authEndpoint='/api/liveblocks-auth'
@@ -13,7 +16,17 @@ const Provider = ({ children }: { children: ReactNode }) => {
         const users = await getClerkUsers({ userIds });
 
         return users;
-      }}>
+      }}
+      resolveMentionSuggestions={async({text, roomId}) => {
+        const roomUsers = await getDocumentUsers({
+          roomId,
+          currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+          text
+        })
+
+        return roomUsers;
+      }}
+      >
       <ClientSideSuspense fallback={<Loader />}>
         {children}
       </ClientSideSuspense>
