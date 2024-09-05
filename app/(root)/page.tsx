d6e595969a842c9f09839882141bbe4029b85aa9
Leonardo Kemisch <leonardokemisch@gmail.com>
2024-09-05 14:39:38 -0300

    feat: delete document

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,6 +7,7 @@ import Image from 'next/image'
 import { getDocuments } from '@/lib/actions/room.actions'
 import Link from 'next/link'
 import { dateConverter } from '@/lib/utils'
+import { DeleteModal } from '@/components/DeleteModal'
 
 const Home = async () => {
 
@@ -35,26 +36,26 @@ const Home = async () => {
               email={clerkUser.emailAddresses[0].emailAddress}
             />
           </div>
-              <ul className='document-ul'>
-                {roomDocuments.data.map(({ id, metadata, createdAt }: any) =>(
-                  <li key={id} className='document-list-item'>
-                    <Link href={`/documents/${id}`} className='flex flex-1 items-center gap-4'>
-                      <div className='hidden rounded-md bg-dark-500 p-2 sm:block'>
-                        <Image 
-                        src='/assets/icons/doc.svg'
-                        alt='file'
-                        width={40}
-                        height={40}/>
-                      </div>
-                      <div className='space-y-1'>
-                        <p className='line-clamp-1 text-lg'>{metadata.title}</p>
-                        <p className='text-sm font-light text-blue-100'>Created about {dateConverter(createdAt)}</p>
-                      </div>
-                    </Link>
-                    {/* TODO: Delete Button */}
-                  </li>
-                ))}
-              </ul>
+          <ul className='document-ul'>
+            {roomDocuments.data.map(({ id, metadata, createdAt }: any) => (
+              <li key={id} className='document-list-item'>
+                <Link href={`/documents/${id}`} className='flex flex-1 items-center gap-4'>
+                  <div className='hidden rounded-md bg-dark-500 p-2 sm:block'>
+                    <Image
+                      src='/assets/icons/doc.svg'
+                      alt='file'
+                      width={40}
+                      height={40} />
+                  </div>
+                  <div className='space-y-1'>
+                    <p className='line-clamp-1 text-lg'>{metadata.title}</p>
+                    <p className='text-sm font-light text-blue-100'>Created about {dateConverter(createdAt)}</p>
+                  </div>
+                </Link>
+                <DeleteModal roomId={id} />
+              </li>
+            ))}
+          </ul>
 
         </div>
       ) : (
--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -4,6 +4,8 @@ import { nanoid } from 'nanoid';
 import { liveblocks } from '../liveblocks';
 import { revalidatePath } from 'next/cache';
 import { getAccessType, parseStringify } from '../utils';
+import { redirect } from 'next/navigation';
+
 
 export const createDocument = async ({ userId, email }:
     CreateDocumentParams) => {
@@ -109,6 +111,16 @@ export const removeCollaborator = async ({ roomId, email }: { roomId: string, em
 
     } catch (error) {
         console.log(`Error happened while removing a collaborator: ${error}`);
+    }
+}
+
+export const deleteDocument = async (roomId: string) => {
+    try {
+        await liveblocks.deleteRoom(roomId);
+        revalidatePath('/');
+        redirect('/');
+    } catch (error) {
+        console.log(`Error happened while deleting a room: ${error}`);
     }
 }
 
